3052438b3124d1f4cc802e748536749ffe1fa925
FANAYA <FANAYA@BLTOMATO.COM>
2018-10-18 16:36:18 -0700

    left of on section5 lecture 61

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_key = 'f0402116df3629fb626b32267d75fede';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_key}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
